01ef9b41cb816a189270a6dae38e067c91c434a2
Raphael Fonseca de Oliveira <49069019+raphaelfo@users.noreply.github.com>
2019-04-05 21:50:19 -0300

    Add files via upload

---
+++ exercicios/53-saldo2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void compra(int* conta, int valor){
+	*conta=*conta-valor;
+}
+int main(void){
+	int minha_conta1;
+	int minha_conta2;
+    int i;
+	int compras[5]={100,50,80,30,20};
+	int* p;
+	printf("digite o saldo conta1: ");
+	scanf("%d",&minha_conta1);
+	printf("digite o saldo conta2: ");
+	scanf("%d",&minha_conta2);
+	for(i=0;i<5;i++){
+    if(minha_conta1>minha_conta2){
+		p=&minha_conta1;
+	}
+	else{
+		p=&minha_conta2;
+	}
+		compra(p,compras[i]);
+		printf("%d %d\n",minha_conta1,minha_conta2);
+	}
+	return 0;
+}
